1e0251ef505b21e38ac10921e5247368eebc49c0
Dagim Asnake <dagimasnake388@gmail.com>
2025-12-25 14:06:19 +0300

    additional changes
    setting up the environment

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 import express from 'express';
+import { PORT} from './config/env.js';
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-const HOSTNAME = process.env.HOSTNAME || '0.0.0.0';
+
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to Dagis world (from  the back end)');
-});
+}) ;
 
-app.listen(PORT, HOSTNAME, () => {
+app.listen( PORT , hostname, () => {
   console.log(`Server is running on port ${PORT}`);
-});
+}); 
